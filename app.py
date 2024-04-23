d20d3973ad39fecbe1fc3d31530ce0786ced777a
Nero <luizhenriquerezio@gmail.com>
2024-04-23 15:44:26 -0300

    feat:primeira implementação do streamlit

--- app.py
+++ app.py
@@ -29,14 +29,16 @@ selecao_depto = st.selectbox("Selecione um dos departamentos", lista_depto)
 if st.button("Imprimir o gráfico"):
     if selecao_depto == 'GERAIS':
         dados = planilha
+        texto = f'Gráfico de "Tipo de Serviços" para {selecao_depto}'
     else:
         dados = planilha[planilha['DEPTO'].isin(lista_depto)]
+        texto = f'Gráfico de "Tipo de Serviços" para o departamento {selecao_depto}'
 
     fig, ax = plt.subplots(figsize=(14, 7))
     ax.barh(dados['TIPO DE SERVIÇO'].astype(str), dados.index)
     ax.set_xlabel('Índice')
     ax.set_ylabel('Tipo de Serviços')
-    ax.set_title(f'Gráfico de "Tipo de Serviços" para o departamento {selecao_depto}')
+    ax.set_title(texto)
     plt.subplots_adjust(left=0.3)
     buf = BytesIO()
     plt.savefig(buf, format='png')
