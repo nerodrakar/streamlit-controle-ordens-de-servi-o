2ebc69ec56f856dd0ebab9eec9dc83cb20122808
Nero <luizhenriquerezio@gmail.com>
2024-04-23 15:41:10 -0300

    feat:primeira implementação do streamlit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 import tempfile
+from io import BytesIO
 
-df = pd.read_excel('Controle Ordens de Serviço ADM.xlsx', sheet_name='Ordens de Serviço - 2024', usecols='A:O')
-planilha = df.copy()
-
+planilha = pd.read_excel('Controle Ordens de Serviço ADM.xlsx', sheet_name='Ordens de Serviço - 2024', usecols='A:O')
+planilha = planilha.copy()
 st.write(planilha)
 
 temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx')
@@ -22,7 +23,22 @@ st.download_button(
     mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 )
 
+lista_depto = sorted(planilha['DEPTO'].astype(str).unique().tolist())
+lista_depto = ['GERAIS'] + lista_depto
+selecao_depto = st.selectbox("Selecione um dos departamentos", lista_depto)
+if st.button("Imprimir o gráfico"):
+    if selecao_depto == 'GERAIS':
+        dados = planilha
+    else:
+        dados = planilha[planilha['DEPTO'].isin(lista_depto)]
 
-selecao_coluns = st.selectbox("Selecione uma das colunas", planilha.columns)
-if st.button("Imprimir a coluna"):
-    st.write(df[selecao_coluns])
+    fig, ax = plt.subplots(figsize=(14, 7))
+    ax.barh(dados['TIPO DE SERVIÇO'].astype(str), dados.index)
+    ax.set_xlabel('Índice')
+    ax.set_ylabel('Tipo de Serviços')
+    ax.set_title(f'Gráfico de "Tipo de Serviços" para o departamento {selecao_depto}')
+    plt.subplots_adjust(left=0.3)
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    st.image(buf)
